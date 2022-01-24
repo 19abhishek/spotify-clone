08880305286d2d6e9a80df68a42e0c7e8e4ae5eb
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-24 22:41:44 +0530

    Search option added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Spotify - Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Dashboard />
