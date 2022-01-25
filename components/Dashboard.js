661108a0d07a964385100b14f06b5515bdb773b7
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-25 19:37:33 +0530

    Tracks fetched from spotify api

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,12 +2,17 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 import Body from "../components/Body";
 import Right from "../components/Right";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+});
 
 function Dashboard() {
   return (
     <main>
       <Sidebar />
-      <Body />
+      <Body spotifyApi={spotifyApi} />
       <Right />
     </main>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,8 @@ export default function Home() {
     return <Loader />;
   }
 
+  console.log(session);
+
   return (
     <div className="">
       <Head>
