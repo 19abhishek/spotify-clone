cb292074cf671b35317abe8546edd2ad3a244a7e
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-26 10:29:40 +0530

    Adding genres component in body

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -3,16 +3,24 @@ import Sidebar from "../components/Sidebar";
 import Body from "../components/Body";
 import Right from "../components/Right";
 import SpotifyWebApi from "spotify-web-api-node";
+import { playingTrackState } from "../atoms/playerAtom";
+import { useRecoilState } from "recoil";
 
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
 });
 
 function Dashboard() {
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+
+  const chooseTrack = (track) => {
+    setPlayingTrack(track);
+  };
+
   return (
     <main>
       <Sidebar />
-      <Body spotifyApi={spotifyApi} />
+      <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
       <Right />
     </main>
   );
